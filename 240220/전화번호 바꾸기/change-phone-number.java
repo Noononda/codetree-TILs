ea6d415a01965c01f1961edd45f6d89f51e7c9f1
설화 <137466623+Noononda@users.noreply.github.com>
2024-02-20 22:25:29 +0900

    Title: 전화번호 바꾸기, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240220/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+        String a = st.nextToken();
+        String b = st.nextToken();
+        String c = st.nextToken();
+
+        System.out.print(a + "-" + c + "-" + b);
+    }
+}
