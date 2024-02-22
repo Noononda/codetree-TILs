b929a042d951469c13853a70a1dd601e869d9a94
설화 <137466623+Noononda@users.noreply.github.com>
2024-02-22 19:58:07 +0900

    Title: 함수를 이용한 소수 판별, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240222/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(sum(a, b));
+    }
+
+    public static int sum(int a, int b) {
+        int total = 0;
+        int count = 0;
+        for (int i = a; i <= b; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                total = total + i;
+            } else {
+                count = 0;
+            }
+        }
+
+        return total;
+    }
+}
