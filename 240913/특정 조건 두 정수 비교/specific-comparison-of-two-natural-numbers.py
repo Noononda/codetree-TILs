1d527653cd58c9ab05ccdfa283689f31196c93f7
설화 <137466623+Noononda@users.noreply.github.com>
2024-09-13 18:40:12 +0900

    Title: 특정 조건 두 정수 비교, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240913/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,14 @@
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+if a < b:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
+
+if a == b:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
