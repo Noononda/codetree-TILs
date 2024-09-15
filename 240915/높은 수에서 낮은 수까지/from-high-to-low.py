38363199d38bf6ab80dfac93b2527e1a9447f744
설화 <137466623+Noononda@users.noreply.github.com>
2024-09-15 19:02:00 +0900

    Title: 높은 수에서 낮은 수까지, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240915/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+if a > b:
+    for i in range(a, b - 1, -1):
+        print(i, end=" ")
+elif b > a:
+    for i in range(b, a - 1, -1):
+        print(i, end=" ")
